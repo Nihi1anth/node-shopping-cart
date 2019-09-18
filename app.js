2d93ba12dec647c0bee6efb9d0ad07ed23c19c56
Nihi1anth <ishaltbecome@gmail.com>
2019-09-19 00:07:16 +0700

    Навигация

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+var ehbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', ehbs({defaultLayout: 'layout', extname: '.hbs'}))
+app.set('view engine', '.hbs');
 
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
