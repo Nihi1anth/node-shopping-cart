f262766be9d40cd9137dc34c8a288e1b52b5fec4
Nihi1anth <ishaltbecome@gmail.com>
2019-10-27 00:19:34 +0700

    vat to const complete

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,16 +1,9 @@
 const express = require('express');
-const multer = require('multer');
 const router = express.Router();
-
-const multerConfig = require('../config/multer.config');
 
 const productCtrl = require('../controllers/product.controller');
 
-const upload = multer({ storage: multerConfig.storageConfig, fileFilter: multerConfig.fileFilterConfig });
-
 /* GET home page. */
 router.get('/', productCtrl.goHome);
-router.get('/upload', productCtrl.getUploadForm);
-router.post('/upload', upload.single('myFile'), productCtrl.postUploadForm);
 
 module.exports = router;
